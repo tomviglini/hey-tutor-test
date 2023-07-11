64e24f780509b2aee8c943aae0ccebf5157309cb
Tom Viglini <tom@viglini.com>
2023-07-10 22:59:01 -0300

    init lighthouse

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('lighthouse');
 
 /*
 |--------------------------------------------------------------------------
@@ -92,8 +93,13 @@ $app->configure('app');
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+
+$app->register(MLL\GraphiQL\GraphiQLServiceProvider::class);
+$app->register(\Nuwave\Lighthouse\LighthouseServiceProvider::class);
+$app->register(\Nuwave\Lighthouse\Pagination\PaginationServiceProvider::class);
+$app->register(\Nuwave\Lighthouse\Validation\ValidationServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
